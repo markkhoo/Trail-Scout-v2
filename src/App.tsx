663c3e562c7d89e5c4b9327c3a17a25c2ce0bcec
markeezy <markkhoo95@gmail.com>
2021-10-08 13:29:14 -0700

    add pins

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
 import GoogleMapReact from 'google-map-react'
 import { Icon } from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/map-marker'
 import './App.css';
+
+interface PinText {
+  lat: number;
+  lng: number;
+  text: string;
+}
+
+const LocationPin: FC<PinText> = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+)
 
 function App() {
 
@@ -14,7 +27,16 @@ function App() {
           defaultCenter={{ lat: 37.42216, lng: -122.08427 }}
           defaultZoom={17}
         >
-
+          <LocationPin
+            lat={37.42216}
+            lng={-122.08427}
+            text={"THIS ROLLING WALTS OF LIFE NEVER SEEMS TO TIRE THE RESTLESS SOUL"}
+          />
+          <LocationPin
+            lat={37.4}
+            lng={-122.1}
+            text={"TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"}
+          />
         </GoogleMapReact>
       </div>
     </div>
