6094eec93654a1eac558fecc323060211470aaf6
markeezy <markkhoo95@gmail.com>
2021-10-09 23:30:50 -0700

    get coord from map

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react'
 import { Icon } from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/map-marker'
 import './App.css';
+import { clear } from 'console';
 
-interface PinText {
+interface PinPoint {
   lat: number;
   lng: number;
-  text: string;
+  text?: string;
 }
 
-const LocationPin: FC<PinText> = ({ text }) => (
+const LocationPin: FC<PinPoint> = ({ text }) => (
   <div className="pin">
     <Icon icon={locationIcon} className="pin-icon" />
     <p className="pin-text">{text}</p>
@@ -18,6 +19,25 @@ const LocationPin: FC<PinText> = ({ text }) => (
 )
 
 function App() {
+  const [getCoord, setCoord] = useState<PinPoint>({ lat: 37.42216, lng: -122.08427 });
+
+  //
+  let timer: NodeJS.Timeout;
+
+  const searchAfterTime = () => {
+    timer = setTimeout(() => {
+      console.log(timer)
+    }, 3000);
+    return () => clearTimeout(timer)
+  };
+
+  const clearSearchAfterTime = () => {
+    clearTimeout(timer)
+  }
+
+  useEffect(() => {
+    console.log(getCoord);
+  }, [getCoord]);
 
   return (
     <div className="App">
@@ -26,16 +46,27 @@ function App() {
           bootstrapURLKeys={{ key: '' }}
           defaultCenter={{ lat: 37.42216, lng: -122.08427 }}
           defaultZoom={17}
+          onDragEnd={(map) => {
+            setCoord({
+              lat: parseFloat(map.center.lat().toFixed(5)),
+              lng: parseFloat(map.center.lng().toFixed(5))
+            });
+
+            clearSearchAfterTime();
+            searchAfterTime();
+
+          }}
+
         >
           <LocationPin
             lat={37.42216}
             lng={-122.08427}
-            text={"THIS ROLLING WALTS OF LIFE NEVER SEEMS TO TIRE THE RESTLESS SOUL"}
+            text={"THIS ROLLING WALTS OF LIFE"}
           />
           <LocationPin
             lat={37.4}
             lng={-122.1}
-            text={"TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"}
+            text={"TTTTTTTTTTT TTTTTTTTTTTT"}
           />
         </GoogleMapReact>
       </div>
