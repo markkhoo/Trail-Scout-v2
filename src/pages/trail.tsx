c166eed6815d46065a124189b34ef0470a5de69c
markeezy <markkhoo95@gmail.com>
2021-10-21 00:49:54 -0700

    use react-dom-router

---
+++ src/pages/trail.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function TrailDetail() {
+
+
+    return (
+        <p>Hello World</p>
+    )
+}
+
+export default TrailDetail
