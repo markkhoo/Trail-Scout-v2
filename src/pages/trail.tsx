b71dd434797bf3be77c4531ac370160a6b3e06ea
markeezy <markkhoo95@gmail.com>
2021-10-26 02:08:44 -0700

    add weather API to trail page

--- src/pages/trail.tsx
+++ src/pages/trail.tsx
@@ -3,12 +3,7 @@ import { Link, useParams } from "react-router-dom";
 
 type TrailID = {
     trailsID: string
-}
-
-// type Coordinates = {
-//     lat: number;
-//     lng: number;
-// }
+};
 
 type SingleTrail = {
     city?: string;
@@ -25,13 +20,60 @@ type SingleTrail = {
     region?: string;
     thumbnail?: string;
     url?: string
+};
+
+type WeatherSub = {
+    description: string;
+    icon: string;
+    id: number;
+    main: string
+};
+
+type DailyWeather = {
+    clouds: number;
+    dew_point: number;
+    dt: number;
+    feels_like: {
+        day: number,
+        eve: number,
+        morn: number,
+        night: number
+    };
+    humidity: number;
+    moon_phase: number;
+    moonrise: number;
+    moonset: number;
+    pop: number;
+    pressure: number;
+    sunrise: number;
+    sunset: number;
+    temp: {
+        day: number;
+        eve: number;
+        max: number;
+        min: number;
+        morn: number; 
+        night: number;
+    };
+    uvi: number;
+    weather: WeatherSub[];
+    wind_deg: number;
+    wind_gust: number;
+    wind_speed: number
+};
+
+type Weather = {
+    daily: DailyWeather[];
+    lat: number;
+    lon: number;
+    timezone: string;
+    timezone_offset: number
 }
 
 function TrailDetail() {
     const trailParam: TrailID = useParams()
     const trailID: string = trailParam.trailsID;
 
-    // const [getCoord, setCoord] = useState<Coordinates>({ lat: 0, lng: 0 });
     const [getTrail, setTrail] = useState<SingleTrail>({
         city: "test",
         country: "test",
@@ -48,18 +90,26 @@ function TrailDetail() {
         thumbnail: "test",
         url: "test"
     });
+    const [getWeather, setWeather] = useState<Weather>({
+        daily: [],
+        lat: 0,
+        lon: 0,
+        timezone: "",
+        timezone_offset: 0
+    })
 
     useEffect(() => {
-        // console.log(trailParam)
-        // console.log(trailID)
         searchTrailByID(trailID)
             .then(res => {
                 setTrail(res);
 
-                const lat:string | undefined = res.lat;
-                const lng:string | undefined = res.lon;
+                const lat: string = res.lat as string;
+                const lng: string = res.lon as string;
 
-                console.log(`lat: ${lat}; lon: ${lng}`)
+                searchWeather(lat, lng)
+                    .then(res => setWeather(res))
+                    .catch(err => console.error(err))
+
             })
             .catch(err => {
                 console.error(err);
@@ -72,6 +122,10 @@ function TrailDetail() {
         console.log(getTrail)
     }, [getTrail])
 
+    useEffect(() => {
+        console.log(getWeather)
+    }, [getWeather])
+
     const searchTrailByID = (trailID: string): Promise<SingleTrail> => fetch(`https://trailapi-trailapi.p.rapidapi.com/trails/${trailID}`, {
         "method": "GET",
         "headers": {
@@ -80,7 +134,11 @@ function TrailDetail() {
         }
     })
         .then((response) => response.json())
-        .then(res => res.data[0] as SingleTrail)
+        .then(res => res.data[0] as SingleTrail);
+
+    const searchWeather = (lat: string = "0", lon: string = "0"): Promise<Weather> => fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${'current,minutely,hourly,alerts'}&units=${'imperial'}&appid=${'22bb6e2db366aab8539ac22df7b32d3a'}`)
+        .then((response) => response.json())
+        .then(res => res as Weather);
 
     return (<>
         <p>Hello World</p>
